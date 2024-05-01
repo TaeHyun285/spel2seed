3fc4a459143c96a0e282b2a80cf5b27d18e87178
taehyun <taehyun285@gmail.com>
2024-05-02 03:33:36 +0900

    position

--- seedtracker.lua
+++ seedtracker.lua
@@ -1,17 +1,5 @@
-while true do
-    local x, y, z = get_position(id)
-    print(type(x))
-    printf("%d %d %d", x, y, z)
+id = get_entities_by(0, MASK.PLAYER, LAYER.FRONT)
+for i, j in pairs(id) do
+    x, y, z = get_render_position(j)
 end
-
-meta.name = 'Script'
-meta.version = '0.1'
-meta.description = 'This script will not be saved anywhere but can be used to test things quickly!'
-meta.author = 'You'
-
-count = 0
-id = set_interval(function()
-  count = count + 1
-  message('Hello from your shiny new script')
-  if count > 4 then clear_callback(id) end
-end, 60)
+printf("%f %f %f", x, y, z)
