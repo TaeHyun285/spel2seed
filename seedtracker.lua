f4f9b87f7ab4119da8d63957e1ccddd9df01e742
taehyun <taehyun285@gmail.com>
2024-05-02 14:12:06 +0900

    warp through world 1

--- seedtracker.lua
+++ seedtracker.lua
@@ -3,13 +3,20 @@ meta.version = "WIP"
 meta.description = "I don't know"
 meta.author = "TaeHyun"
 
+for i = 0, 20, 1 do
+    for j = 2, 3, 1 do 
+        warp(1,j,1)
+        
+    end
+end
+-- get player position
 id = get_entities_by(0, MASK.PLAYER, LAYER.FRONT)
 for i, j in pairs(id) do
     x, y, z = get_render_position(j)
 end
 printf("%f %f %f", x, y, z)
 
+-- find if springshoes exists
 for i,v in ipairs(get_entities_by(ENT_TYPE.ITEM_PICKUP_SPRINGSHOES, MASK.ITEM, LAYER.FRONT)) do
     printf("%f %f",i, v)
-    return true
 end
